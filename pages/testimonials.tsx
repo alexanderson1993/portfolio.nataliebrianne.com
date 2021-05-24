590c9dc201e87c1f2ec57db15b6edfac0861d455
Alex Anderson <alexanderson1993@gmail.com>
2021-05-24 07:42:20 -0400

    Fix 2

--- pages/testimonials.tsx
+++ pages/testimonials.tsx
@@ -1,4 +1,4 @@
-import { Meta } from "@components/Meta blah";
+import { Meta } from "@components/Meta";
 import { Fragment } from "react";
 import Nav from "@components/Nav";
 import Header from "@components/Header";
